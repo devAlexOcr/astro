33c86b9d786925993dee7131217e2e1d523dbd62
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2024-01-04 17:30:35 +0100

    ADD - switch /login

--- backend/index.php
+++ backend/index.php
@@ -1,13 +1,13 @@
 <?php
-      header("Access-Control-Allow-Origin: *");
-      header("Access-Control-Allow-Headers: *");
-      header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header('Content-Type: application/json');
 
-      error_reporting(E_ALL);
-        ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
 
-      require "./middleware/.DbConnect.php";
+    require "./middleware/.DbConnect.php";
 
     $request = $_SERVER['REQUEST_URI'];
 
@@ -17,7 +17,12 @@
             case '/add':
                     require __DIR__ . $viewDir . '.add.php';
                 break;
-            
+            case '/get':
+                    require __DIR__ . $viewDir . '.getMessier.php';
+                break;
+            case '/login':
+                    require __DIR__ . $viewDir . '.users.php';
+                break;
             default:
                     http_response_code(204); require __DIR__ . $viewDir . '.404.php';
                 break;
