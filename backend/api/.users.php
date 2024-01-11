2299328fb62a6d8883b8d8b6c16993f9751d484d
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2024-01-11 18:02:31 +0100

    ADD - Update image Messier avec image User

--- backend/api/.users.php
+++ backend/api/.users.php
@@ -1,10 +1,12 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
+
         $postData = json_decode(file_get_contents("php://input"), true);
-            if($postData === null) {
-                throw new Exception("Erreur lors de la lecture des données JSON du login.");
-            }
+
+        if($postData === null) {
+            throw new Exception("Erreur lors de la lecture des données JSON du login.");
+        }
 
         $Pseudo = htmlspecialchars($postData['pseudo']);
         $Password = password_hash($postData['password'], PASSWORD_DEFAULT);
@@ -34,7 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 // Creation de la Table $postData['pseudo']
 
                 $queryCreateTable = "CREATE TABLE " . $postData['pseudo'] . "Messier" . " (
-                    IdMessier INT(11) NOT NULL, 
+                    IdMessier INT(11) PRIMARY KEY NOT NULL, 
                     Image BLOB NOT NULL,
                     date DATE NOT NULL
                 )";
@@ -45,30 +47,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 $rep[0]['pseudo'] = $Pseudo;
                 $rep[0]['id'] = $lastId;
                 $rep[1]['SQL'] = "Table created";
-                
             }
+            
             if (count($data) === 1) {
                 $hash = $data[0]["Password"];
                 if(password_verify($postData['password'], $hash)) {
 
-                    // JOIN les table users et messiers
+                    $queryValue = '../../public/assets/images/scopes.png';
+
+
+                    //JOIN les table users et messiers
+
+                    $queryUpdateImage = "UPDATE Messier INNER JOIN " . $postData['pseudo'] . "Messier" . " ON Messier.IdMessier = " . $postData['pseudo'] . "Messier" . ".IdMessier SET Messier.Image = " . $postData['pseudo'] . "Messier.Image";
+    
+                    $statement = $connect->prepare($queryUpdateImage);
+                    $statement->execute();
 
                     $rep[0]['message'] = "Utilisateur reconnu";
                     $rep[0]['status'] = "Connected";
-                    
+                    $rep[1]['SQL'] = "success ! image is updated"; 
+
                 } else {                 
                     $rep[0]['message'] = "paire Pseudo / Password incorrect";
                     $rep[0]['status'] = "Disconnect";
-                }
-
-                
-            }
+                }   
+            } 
         }
-        catch (PDOException $e) {
+    
+    catch (PDOException $e) {
             $res = array(
                 "error" => "Erreur lors login : " . $e->getMessage()
             ); 
-        }
-        echo json_encode($rep);
-}
+        }  
+        echo json_encode($rep); 
+    }
+ 
 ?>
