50f9ecdaa9f743e07407c0689e68855cf913221b
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2024-01-15 16:48:36 +0100

    ADD - Integration page 404 error

--- src/rooter.jsx
+++ src/rooter.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home.jsx';
 import Messier from './pages/Messier.jsx';
 import ObjetMessier from './pages/ObjetMessier.jsx';
 import Users from './pages/Users.jsx';
+import Error from './pages/404.jsx';
 
 import './rooter.css'
 
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/messier' element={<Messier />} />
         <Route path='/messier/:id' element={<ObjetMessier />} />
         <Route path='/login' element={<Users />} />
+        <Route path='/*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
